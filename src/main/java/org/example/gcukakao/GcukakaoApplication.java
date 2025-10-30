c9e32c2eff4af8a68bf60c999aac502188bfb1e5
js2000 <js2000@sbsystems.co.kr>
2025-10-30 09:56:10 +0900

    Initial commit: add Jenkins pipeline project

---
+++ src/main/java/org/example/gcukakao/GcukakaoApplication.java
@@ -0,0 +1,13 @@
+package org.example.gcukakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcukakaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcukakaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gcukakao/GcukakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gcukakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcukakaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
